012411c41cf41d1445c50bac0027878f5206f8d1
Gene Magee <genejmagee@gmail.com>
2016-03-08 23:06:15 +0000

    added links to user profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-   
+   # form were user can fill out their own profile
+   #look into dbase and getthe current logged in user
+   def new
+       @user = User.find( params[:user_id] )
+       @profile = @user.build_profile
+   end
 end
